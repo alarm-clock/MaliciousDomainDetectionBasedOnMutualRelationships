9e57852064019315e2ac341a5e8f0a42d9c81d9c
Michal <xbukas00@stud.fit.vutbr.cz>
2025-09-26 23:21:53 +0200

    Final fix to the copy script

--- run_scripts/copy_datasets_to_scratch.sh
+++ run_scripts/copy_datasets_to_scratch.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 jq -r '.[] | "\(.use)|\(.path)"' $DATA_DIR/s_dataset_config.json | while IFS='|' read -r use path; do
-  echo $path
   if [ "$use" = "true" ]; then
-    echo Coping file $path	  
-    cp $DATA_DIR/$path $SRATCHDIR
+    echo Copying file $path
+    cp $DATA_DIR/$path $SCRATCHDIR
   fi
 done
 
 rm $SCRATCHDIR/dataset_config.json
 
 jq '[.[] | select(.use == true)]' $DATA_DIR/s_dataset_config.json > $SCRATCHDIR/tmp.json
-jq --arg p "$SCRATCHDIR" '.[] | .path = ($p + "/" + .path)' $SCRATCHDIR/tmp.json > $SCRATCHDIR/dataset_config.json
+jq --arg p "$SCRATCHDIR" '[.[] | .path = ($p + "/" + .path)]' $SCRATCHDIR/tmp.json > $SCRATCHDIR/dataset_config.json
 
 rm $SCRATCHDIR/tmp.json
 
