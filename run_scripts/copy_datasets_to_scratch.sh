2f7c5caf76c0a745d709b2937f075af066901de7
Michal <xbukas00@stud.fit.vutbr.cz>
2025-09-26 22:55:47 +0200

    Fixing copy script bugs

--- run_scripts/copy_datasets_to_scratch.sh
+++ run_scripts/copy_datasets_to_scratch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 jq -r '.[] | "\(.use) \(.path)"' $DATA_DIR/s_dataset_config.json | while IFS= read -r use path; do
-  if [ "$use" = "true"]; then
+  echo $path
+  if [ "$use" = "true" ]; then
+    echo Coping file $path	  
     cp $DATA_DIR/$path $SRATCHDIR
   fi
 done
@@ -9,7 +11,7 @@ done
 rm $SCRATCHDIR/dataset_config.json
 
 jq '[.[] | select(.use == true)]' $DATA_DIR/s_dataset_config.json > $SCRATCHDIR/dataset_config.json
-jq --arg p "$SCRATCHDIR" '.[] | .path = ($p + "/" + .path)' $SCRATCHDIR/dataset_config.json
+jq --arg p "$SCRATCHDIR" '.[] | .path = ($p + "/" + .path)' $SCRATCHDIR/dataset_config.json 
 
 
 
