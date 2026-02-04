eceb8785e6dde16f58e842bcb5f876efd43b0396
Michal <xbukas00@stud.fit.vutbr.cz>
2026-02-04 15:28:35 +0100

    Finished cpp compilation scripts and import shanenigans

--- run_scripts/compile_on_node.sh
+++ run_scripts/compile_on_node.sh
@@ -10,7 +10,7 @@ RUN_ID="$(date +%Y%m%d_%H%M%S)_$$"
 ARCH="$(uname -m)"
 RUN_NAME=$1
 
-BUILD_DIR="${SCRATCH}/${RUN_NAME}_${RUN_ID}${ARCH}"
+export BUILD_DIR="${SCRATCH}/${RUN_NAME}_${RUN_ID}${ARCH}"
 mkdir -p "${BUILD_DIR}"
 
 python3 $SETUP_SCRIPT_PATH build_ext --build-lib "${BUILD_DIR}" --build-temp "${BUILD_DIR}/temp"
--- run_scripts/singularity_exec.sh
+++ run_scripts/singularity_exec.sh
@@ -10,6 +10,8 @@ RUN_SCRIPT=$PROJECT_DIR/testmain.py
 
 #$PROJECT_DIR/run_scripts/mongostart.sh
 
-./compile_on_node.sh testing_nodes
-python3 $RUN_SCRIPT --dglformat $GRAPH_EXPORT_PATH/hetero_full_all_new_domains.dglg --log_file $LOG_EXPORT_PATH/testin_new_domains.log --demo_from_list $DATASET_IMPORT_PATH/classes_main.txt
+. $PROJECT_DIR/run_scripts/compile_on_node.sh cpp_comp_dir
+python3 $RUN_SCRIPT --dglformat $GRAPH_EXPORT_PATH/hetero_full_all_new_domains.dglg --log_file $LOG_EXPORT_PATH/testin_new_domains_cpp.log --demo_from_list $DATASET_IMPORT_PATH/classes_main.txt
+rm -rf $BUILD_DIR #build dir is from the compile_on_node_script
+
 #$PROJECT_DIR/run_scripts/mongostop.sh
