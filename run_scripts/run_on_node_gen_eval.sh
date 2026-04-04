b06a221d6588d0ec57c6831663248fc056b507db
Michal <xbukas00@stud.fit.vutbr.cz>
2026-04-04 15:59:58 +0200

    Added option to not generate new graph and use existing one

--- run_scripts/run_on_node_gen_eval.sh
+++ run_scripts/run_on_node_gen_eval.sh
@@ -9,4 +9,4 @@ CONTAINER=$BRNO_HOME/images/dgl_mongo_cpp_neo4j.sif
 PROJECT_DIR=$BRNO_HOME/diplomka/DeepWalkTesting
 RUN_SCRIPT=$PROJECT_DIR/testmain.py
 trap 'clean_scratch' TERM EXIT
-singularity exec $MONGO_SING_BIND $PROXY_BIND $CONTAINER $PROJECT_DIR/run_scripts/singularity_exec_gen_eval.sh
+singularity exec $MONGO_SING_BIND $PROXY_BIND $CONTAINER $PROJECT_DIR/run_scripts/singularity_exec_gen_eval.sh $@
